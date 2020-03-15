16272966f5dae0c4b2d57e803c64451572807198
Atul R <atulanand94@gmail.com>
2020-03-15 12:57:16 +0100

    Adds clipboard image example

---
+++ nodegui/clipboard-image/README.md
@@ -0,0 +1,11 @@
+# Clipboard image reading and writing app
+
+This example showcases how to build a nodegui app that can read and write images to clipboard.
+
+To run the demo:
+
+0. `cd clipboard-image`
+
+1. `npm install`
+
+1. `npm start`
---
+++ nodegui/clipboard-image/src/index.ts
@@ -0,0 +1,53 @@
+import {
+  QMainWindow,
+  QWidget,
+  QPushButton,
+  QLineEdit,
+  FlexLayout,
+  QApplication,
+  QClipboardMode,
+  QLabel,
+  QPixmap
+} from "@nodegui/nodegui";
+
+const win = new QMainWindow();
+const center = new QWidget();
+const label = new QLabel();
+const textInput = new QLineEdit();
+const getBtn = new QPushButton();
+const setBtn = new QPushButton();
+
+//----------
+label.setText(
+  "Copy any image onto the clipboard and click `Get clipbard image button`"
+);
+getBtn.setText("Get clipboard image");
+getBtn.addEventListener("clicked", () => {
+  const clip = QApplication.clipboard();
+  const pixmap = clip.pixmap(QClipboardMode.Clipboard);
+  label.setPixmap(pixmap);
+});
+
+//--------------
+textInput.setPlaceholderText(
+  "Enter absolute image path to load into clipboard"
+);
+setBtn.setText("Set clipboard image");
+setBtn.addEventListener("clicked", () => {
+  const clip = QApplication.clipboard();
+  const pixmap = new QPixmap();
+  pixmap.load(textInput.text());
+  clip.setPixmap(pixmap, QClipboardMode.Clipboard);
+  label.setText(`Loaded image at ${textInput.text()} to global clipboard`);
+});
+
+center.setLayout(new FlexLayout());
+center.layout?.addWidget(textInput);
+center.layout?.addWidget(setBtn);
+center.layout?.addWidget(getBtn);
+center.layout?.addWidget(label);
+center.setInlineStyle(`width: 400; height: 400;`);
+win.setCentralWidget(center);
+win.show();
+win.setFixedSize(400, 400);
+(global as any).win = win;
